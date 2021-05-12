07de86e41c42acb233f963b1f363927368c68d4b
dbumiller <d.m.bumiller@gmail.com>
2021-05-11 23:22:47 -0700

    Started working on a relational database

---
+++ db/index.js
@@ -0,0 +1,8 @@
+const {Sequelize, DataTypes} = require('sequelize');
+
+const sequelize = new Sequelize('to be determined', 'root', '') {
+  host: 'localhost',
+  dialect: 'mysql'
+};
+
+module.exports = MULTIPLE THINGS??
