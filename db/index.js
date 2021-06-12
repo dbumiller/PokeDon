b3024caf3104512985af8b6b4df656350da6f854
dbumiller <d.m.bumiller@gmail.com>
2021-06-11 17:41:47 -0700

    Testing out changing datatypes in sequelize

--- db/index.js
+++ db/index.js
@@ -24,116 +24,116 @@ const Pokemon = sequelize.define('pokemon', {
     type: DataTypes.ARRAY(DataTypes.STRING),
     allowNull: false
   },
-  defensiveUtility: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  resistances: {
-    type:DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false
-  },
-  weaknesses: {
-    type: DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false
-  },
-  offensiveSE: {
-    type: DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false
-  },
-  offensiveNVE: {
-    type: DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false
-  },
-  offensiveEffectiveness: {
-    type: DataTypes.JSON,
-    allowNull: false
-  },
-  defensiveEffectiveness: {
-    type: DataTypes.JSON,
-    allowNull: false
-  },
-  offensiveSynergy: {
-    type: DataTypes.ARRAY,
-    allowNull: false
-  },
-  offensiveCompliment: {
-    type: DataTypes.ARRAY,
-    allowNull: false
-  },
-  wallbreaker: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  sweeper: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  hazardRemoval: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  rocker: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  spiker: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  tSpiker: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  webber: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  screener: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  priority: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  momentum: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  speedControl: {
-    type:DataTypes.BOOLEAN,
-    allowNull: false
-  }
-}, {
+  // defensiveUtility: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // resistances: {
+  //   type:DataTypes.ARRAY(DataTypes.STRING),
+  //   allowNull: false
+  // },
+  // weaknesses: {
+  //   type: DataTypes.ARRAY(DataTypes.STRING),
+  //   allowNull: false
+  // },
+  // offensiveSE: {
+  //   type: DataTypes.ARRAY(DataTypes.STRING),
+  //   allowNull: false
+  // },
+  // offensiveNVE: {
+  //   type: DataTypes.ARRAY(DataTypes.STRING),
+  //   allowNull: false
+  // },
+  // offensiveEffectiveness: {
+  //   type: DataTypes.JSON,
+  //   allowNull: false
+  // },
+  // defensiveEffectiveness: {
+  //   type: DataTypes.JSON,
+  //   allowNull: false
+  // },
+  // offensiveSynergy: {
+  //   type: DataTypes.ARRAY(DataTypes.ARRAY),
+  //   allowNull: false
+  // },
+  // offensiveCompliment: {
+  //   type: DataTypes.ARRAY(DataTypes.ARRAY),
+  //   allowNull: false
+  // },
+  // wallbreaker: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // sweeper: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // hazardRemoval: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // rocker: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // spiker: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // tSpiker: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // webber: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // screener: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // priority: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // momentum: {
+  //   type: DataTypes.BOOLEAN,
+  //   allowNull: false
+  // },
+  // speedControl: {
+  //   type:DataTypes.BOOLEAN,
+  //   allowNull: false
+  // }
+// }, {
   timestamps: false
 });
 
-const Team = sequelize.define('team', {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  typeMatchups: {
-    type: DataTypes.JSON,
-    allowNull: false
-  }
-}, {
-  timestamps: false
-});
+// const Team = sequelize.define('team', {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//     primaryKey: true,
+//     autoIncrement: true
+//   },
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   typeMatchups: {
+//     type: DataTypes.JSON,
+//     allowNull: false
+//   }
+// }, {
+//   timestamps: false
+// });
 
 
-Pokemon.belongsTo(Team);
-Team.hasMany(Pokemon);
+// Pokemon.belongsTo(Team);
+// Team.hasMany(Pokemon);
 
 
 module.exports = {
   sequelize,
   Pokemon,
-  Team
+  // Team
 };
--- db/seed.js
+++ db/seed.js
@@ -1,2 +1,6 @@
 const db = require('./index.js');
 const metagame = require('./typingAnalyzer');
+
+const seed = () => {
+  db.Pokemon.bulkCreate(metagame);
+}
