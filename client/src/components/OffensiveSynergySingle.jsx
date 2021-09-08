6d882eb09b9f64b2fda8d6ece97edbc5df2ec158
dbumiller <d.m.bumiller@gmail.com>
2021-09-07 17:35:23 -0700

    Implemented singular offensive synergy lists

--- client/src/components/OffensiveSynergySingle.jsx
+++ client/src/components/OffensiveSynergySingle.jsx
@@ -12,7 +12,17 @@ class OffensiveSynergySingle extends React.Component {
   render() {
     return (
       <div>
-        OffensiveSynergySingle
+        {this.props.pokemon.name} Offensive Synergy
+        <ul>
+          {this.props.pokemon.offensiveSynergy.map((pokemon, index) => {
+            if (Number(pokemon[0]) > 0) {
+              return (
+                <div key={index}>{pokemon[0]} {pokemon[1]}</div>
+              )
+            }
+          })}
+        </ul>
+        <br></br>
       </div>
     )
   }
