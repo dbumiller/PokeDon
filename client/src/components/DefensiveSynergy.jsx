3c17009d181a5ebc7df20af81ce65366640bb09b
dbumiller <d.m.bumiller@gmail.com>
2021-09-08 19:57:29 -0700

    Started working on defensive values for individual pokemon. Having issues making one of the values a weighted number

--- client/src/components/DefensiveSynergy.jsx
+++ client/src/components/DefensiveSynergy.jsx
@@ -68,9 +68,56 @@ class DefensiveSynergy extends React.Component {
       output += key + ':  ' + this.state.defenseChart[key] + ', ';
     }
 
+    var sortedChart = [];
+    for (var key in this.state.defenseChart) {
+      sortedChart.push([this.state.defenseChart[key], key]);
+    }
+
+    sortedChart.sort(function(b, a) {
+      if (a > b) {
+        return -1;
+      } else if (a < b) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    var bottomThird = [];
+    var bottomFour = [];
+
+    for (var i = 0; i < sortedChart.length; i++) {
+      if (sortedChart[i][0] <= sortedChart[5][0]) {
+        bottomThird.push([sortedChart[i][0], sortedChart[i][1]]);
+      }
+      if (sortedChart[i][0] <= sortedChart[3][0]) {
+        bottomFour.push([sortedChart[i][0], sortedChart[i][1]]);
+      }
+    }
+
+    var arrayForSorting = [];
+    for (var i = 0; i < this.state.allPokemon.length; i++) {
+      var current = [];
+      var bottomFourValue = 0;
+      for (var j = 0; j < bottomFour.length; j++) {
+        for (var k = 0; k < this.state.allPokemon[i].defensiveResist.length; k++) {
+          if (bottomFour[j][1] === this.state.allPokemon[i].defensiveResist[k]) {
+            bottomFourValue += Math.abs(bottomFour[j][0]);
+          }
+        }
+        for (var k = 0; k <this.state.allPokemon[i].defensiveWeak.length; k++) {
+          if (bottomFour[j][1] === this.state.allPokemon[i].defensiveWeak[k]) {
+            bottomFourValue += bottomFour[j][0];
+          }
+        }
+      }
+
+      console.log(this.state.allPokemon[i].name, bottomFourValue, bottomFour);
+    }
+
     return (
       <div>
-        defense
+        Defense: Below is your team's type chart followed by a list of pokemon sorted by how much they fill your team's defensive needs. The numbers for any given pokemon are sorted by importance, with most important on the left. The first number for each pokemon is weighted and represents how the pokemon fills in your defense against your biggest type weaknesses. The second number is weighted and represents how much the pokemon takes advantage of your team's defensive strengths. The third number is not weighted and represents how many of your weaker type matchups the pokemon resists.
         <br></br>
         <button onClick={this.goBack}>Back</button>
         <div>
