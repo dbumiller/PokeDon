3f682153f499d3635dd4b70088ee134e7bf8fb44
dbumiller <d.m.bumiller@gmail.com>
2021-09-10 12:52:45 -0700

    Based on feedback, I tweaked the description on the momentum page

--- client/src/components/Momentum.jsx
+++ client/src/components/Momentum.jsx
@@ -19,7 +19,7 @@ class Momentum extends React.Component {
   render() {
     return (
       <div>
-        Momentum Followup: How much the checks to your momentum-mon are likely to be threatened out by the paired pokemon. The higher the number, the more likely the paired pokemon will have a type advantage after you use your momentum move. Think of Abomasnow coming in off of a Vaporeon's Flip Turn. It would having a type advantage against the Grass, Dragon, and Water types that want to switch into Vaporeon.
+        Momentum Followup: How much the checks to your momentum-mon are likely to be threatened out by the paired pokemon. The higher the number, the more likely the paired pokemon will have a type advantage after you use your momentum move. Think of Abomasnow coming in off of a Vaporeon's Flip Turn. It would having a type advantage against the Grass, Dragon, and Water types that want to switch into Vaporeon. Momentum Leadin is the other side of this coin, how well certain pokemon set this one up to start its offense.
         <br></br>
         <button onClick={this.goBack}>Back</button>
         <ul className="teamRoster">
