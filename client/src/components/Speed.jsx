96192c680279a12c73caaac2621ba59bfb5e2347
dbumiller <d.m.bumiller@gmail.com>
2021-09-21 16:46:48 -0700

    Implemented a sorting algorithm so that the speed page renders the team from slowest to fastest

--- client/src/components/Speed.jsx
+++ client/src/components/Speed.jsx
@@ -5,7 +5,7 @@ class Speed extends React.Component {
     super(props);
 
     this.state = {
-
+      speeds: []
     }
     this.goBack = this.goBack.bind(this);
   }
@@ -15,17 +15,34 @@ class Speed extends React.Component {
     this.props.changeView('home');
   }
 
+  // componentDidMount() {
+  //   var speeds = [];
+  //   for (var i = 0; i < this.props.pokemon.length; i++) {
+  //     speeds.push([this.props.pokemon[i].speed, this.props.pokemon[i].name]);
+  //   }
+  //   speeds.sort();
+  //   this.setState({
+  //     speeds: speeds
+  //   })
+  // }
+
   render() {
+    var speeds = [];
+    for (var i = 0; i < this.props.pokemon.length; i++) {
+      speeds.push([this.props.pokemon[i].speed, this.props.pokemon[i].name]);
+    }
+    speeds.sort();
+
     return (
     <div>
       Speed
       <br></br>
       <button onClick={this.goBack}>Back</button>
       <ul className="speeds">
-        {this.props.pokemon.map((pokemon, index) => {
+        {speeds.map((pokemon, index) => {
           return (
-            <div>
-              {pokemon.name} {pokemon.speed}
+            <div key={index}>
+              {pokemon[1]} {pokemon[0]}
             </div>
           )
         })}
