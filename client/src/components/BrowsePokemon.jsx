b902f20f763774387ee445197a4d0e5a30fc702d
dbumiller <d.m.bumiller@gmail.com>
2021-09-21 22:10:37 -0700

    Implemented search functionality to the browse page

--- client/src/components/BrowsePokemon.jsx
+++ client/src/components/BrowsePokemon.jsx
@@ -7,18 +7,22 @@ class BrowsePokemon extends React.Component {
     super(props);
 
     this.state = {
-      pokemon: []
+      pokemon: [],
+      currentPokemon: [],
+      search: ''
     }
 
     this.getPokemon = this.getPokemon.bind(this);
     this.goBack = this.goBack.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
   getPokemon() {
     axios.get('/api/pokemon')
       .then((results) => {
         this.setState({
-          pokemon: results.data
+          pokemon: results.data,
+          currentPokemon: results.data
         })
       })
       .catch((err) => {
@@ -35,14 +39,41 @@ class BrowsePokemon extends React.Component {
     this.props.changeView('home');
   }
 
+  handleInput(e) {
+    if (e.target.value === '') {
+      this.setState({
+        search: '',
+        currentPokemon: this.state.pokemon
+      })
+    } else {
+      var search = e.target.value[0].toUpperCase() + e.target.value.substring(1);
+      this.setState({
+        search: search
+      })
+        var matches = [];
+        var len = search.length;
+        for (var i = 0; i < this.state.pokemon.length; i++) {
+          if (this.state.pokemon[i].name.substring(0, len) === search) {
+            matches.push(this.state.pokemon[i]);
+          }
+        }
+        this.setState({
+          currentPokemon: matches
+        })
+    }
+  }
+
   render() {
     return (
       <div>
         browse
         <br></br>
         <button onClick={this.goBack}>Back</button>
+        <label>Search
+          <input onChange={this.handleInput} value={this.state.search}></input>
+        </label>
         <ul className="pokemon">
-          {this.state.pokemon.map((pokemon, index) => {
+          {this.state.currentPokemon.map((pokemon, index) => {
             return (
               <BrowseElement pokemon={pokemon} key={index} teamId={this.props.teamId}/>
             )
