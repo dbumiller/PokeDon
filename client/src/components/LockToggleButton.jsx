b23b408be9c801b9c6b7983189ea92622bbca9fb
dbumiller <d.m.bumiller@gmail.com>
2021-10-08 15:50:59 -0700

    Implemented a button to toggle the locked status of pokemon inside of the my team page

--- client/src/components/LockToggleButton.jsx
+++ client/src/components/LockToggleButton.jsx
@@ -9,15 +9,48 @@ class LockToggleButton extends React.Component {
       locked: false
     }
 
+    this.lockPokemon = this.lockPokemon.bind(this);
+    this.unlockPokemon = this.unlockPokemon.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      locked: this.props.pokemon.locked
+    })
+  }
+
+  lockPokemon() {
+    axios.put(`/api/pokemon/lock/${this.props.pokemon.name}`)
+    .then(() => {
+      this.setState({
+        locked: true
+      })
+    })
+  }
+
+  unlockPokemon() {
+    axios.put(`/api/pokemon/unlock/${this.props.pokemon.name}`)
+    .then(() => {
+      this.setState({
+        locked: false
+      })
+    })
   }
 
   render() {
-    console.log(this.props.pokemon.locked)
-    return (
-      <span>
-        <button>{this.props.pokemon.locked}</button>
-      </span>
-    )
+    if (this.state.locked) {
+      return (
+        <span>
+          <button onClick={this.unlockPokemon}>Locked. Click to unlock</button>
+        </span>
+      )
+    } else {
+      return (
+        <span>
+          <button onClick={this.lockPokemon}>Unlocked. Click to lock</button>
+        </span>
+      )
+    }
   }
 }
 
--- db/models.js
+++ db/models.js
@@ -58,6 +58,7 @@ const pokemon = {
     return db.Pokemon.findAll();
   },
   lock: (name) => {
+    console.log('hey')
     return db.Pokemon.update({locked: true}, {
       where: {
         name: name
@@ -65,6 +66,7 @@ const pokemon = {
     });
   },
   unlock: (name) => {
+    console.log('you')
     return db.Pokemon.update({locked: false}, {
       where: {
         name: name
