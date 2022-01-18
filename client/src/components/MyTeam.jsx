b95bd52163afd709720db47f77b04a0b65d615fe
dbumiller <d.m.bumiller@gmail.com>
2022-01-18 11:33:55 -0800

    Implemented a checklist on the myTeam page

--- client/src/components/MyTeam.jsx
+++ client/src/components/MyTeam.jsx
@@ -17,6 +17,52 @@ class MyTeam extends React.Component {
   }
 
   render() {
+    var teamProperties = {
+      rocker: false,
+      hazardControl: false,
+      speedControl: false,
+      groundedPoison: false,
+      priority: false,
+      wallbreaker: false,
+      sweeper: false
+    };
+    for (var i = 0; i < this.props.pokemon.length; i++) {
+      if (this.props.pokemon[i].rocker) {
+        teamProperties.rocker = true;
+      }
+      if (this.props.pokemon[i].hazardControl) {
+        teamProperties.hazardControl = true;
+      }if (this.props.pokemon[i].speedControl) {
+        teamProperties.speedControl = true;
+      }if (this.props.pokemon[i].priority) {
+        teamProperties.priority = true;
+      }if (this.props.pokemon[i].wallbreaker) {
+        teamProperties.wallbreaker = true;
+      }if (this.props.pokemon[i].sweeper) {
+        teamProperties.sweeper = true;
+      }
+      for (var j = 0; j < this.props.pokemon[i].typing.length; j++) {
+        if (this.props.pokemon[i].typing[j] === "poison") {
+          var grounded = true;
+          for (var k = 0; k < this.props.pokemon[i].defensiveResist.length; k++) {
+            if (this.props.pokemon[i].defensiveResist[k] === "ground") {
+              grounded = false;
+            }
+          }
+          if (grounded) {
+            teamProperties.groundedPoison = true;
+          }
+        }
+      }
+    }
+    var infoString = '';
+    for (var property in teamProperties) {
+      if (!teamProperties[property]) {
+        infoString += 'You may want to work on getting a ' + property + ' pokemon. ';
+      }
+    }
+
+
     return (
       <div>
         {this.props.teamName}
@@ -31,7 +77,9 @@ class MyTeam extends React.Component {
             )
           })}
         </ul>
+        {infoString}
         <button onClick={this.goBack}>Back</button>
+        <br></br>
       </div>
     )
   }
